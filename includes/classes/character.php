67d4fee0d84291319de31f2dc935141948aad4a3
mrperson123 <mrperson123@users.noreply.github.com>
2019-06-10 21:08:25 +0200

    Update character.php

--- includes/classes/character.php
+++ includes/classes/character.php
@@ -6,7 +6,7 @@ class Character{
     
     public function read(){
         $stmt = db::$conn->prepare("SELECT * FROM ecc_characters");
-		$res = $stmt->execute();
+	$res = $stmt->execute();
         $res = $stmt->fetchAll(PDO::FETCH_ASSOC); 
         
         return $res;
