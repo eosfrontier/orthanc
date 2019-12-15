d165c7b1a4946c88fd5cea2f41643e84102b3f4c
unknown <rolfsiebers@gmail.com>
2019-12-15 15:07:02 +0100

    Figurant add

---
+++ figurant/add/index.php
@@ -0,0 +1,24 @@
+<?php 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+$post = json_decode(file_get_contents('php://input'), true);
+
+include_once "../../includes/include.php";
+include_once "../../includes/token.php";
+
+$cFigurant = new figurant();
+
+if(empty($post["figurant"])){
+    http_response_code(400);
+    echo json_encode("You haven't included meta.");
+    die();
+}
+
+$aFigurant  = $post["figurant"];
+
+$aResult = $cFigurant->addFigurant($aFigurant);
+    http_response_code(200);
+    echo json_encode($aResult);
+die(); 
+
+?>
