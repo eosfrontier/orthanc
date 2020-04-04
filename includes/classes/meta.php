9cd4bd28677802054f56dcf67a26c6c76913b9a7
Willem Monsuwe <willem@monsuwe.nl>
2020-04-04 13:30:07 +0200

    Support for multiple meta values

--- includes/classes/meta.php
+++ includes/classes/meta.php
@@ -25,15 +25,14 @@ class meta{
 
     function updateMeta($id, $metas){
         foreach($metas as $meta){
-            $stmt = database::$conn->prepare("SELECT id, name, value FROM ecc_meta_character WHERE character_id = ? AND name = ?");
-            $res = $stmt->execute(array($id, $meta["name"]));
-            $count = $stmt->rowCount();
-            if($count > 0){
-
+            if (array_key_exists("oldvalue", $meta)) {
+                $stmt = database::$conn->prepare("UPDATE ecc_meta_character SET value=? WHERE character_id = ? AND name = ? AND value = ?");
+                $res = $stmt->execute(array($meta["value"], $id, $meta["name"], $meta["oldvalue"]));
+            } else {
                 $stmt = database::$conn->prepare("UPDATE ecc_meta_character SET value=? WHERE character_id = ? AND name = ?");
                 $res = $stmt->execute(array($meta["value"], $id, $meta["name"]));
-
-            }else{
+            }
+            if ($stmt->rowCount() == 0) {
                 $stmt = database::$conn->prepare("INSERT into ecc_meta_character (value, character_id, name) VALUES (?, ?, ?)");
                 $res = $stmt->execute(array($meta["value"], $id, $meta["name"]));
             }
@@ -45,13 +44,16 @@ class meta{
     function deleteMeta($id, $metas){
         $totcount = 0;
         foreach($metas as $meta){
-            $stmt = database::$conn->prepare("DELETE FROM ecc_meta_character WHERE character_id = ? AND name = ?");
-            $res = $stmt->execute(array($id, $meta["name"]));
+            if (array_key_exists("value", $meta)) {
+                $stmt = database::$conn->prepare("DELETE FROM ecc_meta_character WHERE character_id = ? AND name = ? AND value = ?");
+                $res = $stmt->execute(array($id, $meta["name"], $meta["value"]));
+            } else {
+                $stmt = database::$conn->prepare("DELETE FROM ecc_meta_character WHERE character_id = ? AND name = ?");
+                $res = $stmt->execute(array($id, $meta["name"]));
+            }
             $count = $stmt->rowCount();
             if($count > 0){
                 $totcount += $count;
-
-
             }
         }
 
