6c13a7878ab85f07c4b2f01d90dcde7873c3d9ab
WafflesMcDuff <goldjd@gmail.com>
2020-05-16 10:10:45 -0400

    allow methods and headers

--- includes/include.php
+++ includes/include.php
@@ -28,6 +28,8 @@ $APP["root"] = $_SERVER["DOCUMENT_ROOT"] . $APP["header"]; // define the root fo
 //Inject Headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 $method = $_SERVER['REQUEST_METHOD']; //Grab HTTP REST Method
 $input = json_decode(file_get_contents('php://input'), true); //Store Input
 if (!isset($input)) {
