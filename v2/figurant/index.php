2689c4706b996531de49c61811b9181541f655a2
WafflesMcDuff <goldjd@gmail.com>
2020-05-16 14:09:17 -0400

    Update index.php

--- v2/figurant/index.php
+++ v2/figurant/index.php
@@ -2,6 +2,6 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, cache-control');
 echo 'Success';
 ?>
