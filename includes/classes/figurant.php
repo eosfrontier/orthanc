3bc166852cfe1e71b859240f99f009f37f07439e
WafflesMcDuff <goldjd@gmail.com>
2020-11-17 00:18:34 -0500

    fix figurant class

--- includes/classes/figurant.php
+++ includes/classes/figurant.php
@@ -1,7 +1,6 @@
 <?php
 
 class figurant{
-    
     public function getAll(){
         $stmt = database::$conn->prepare("SELECT * FROM ecc_characters WHERE status LIKE 'figurant%'");
 		$res = $stmt->execute();
@@ -139,4 +138,89 @@ class figurant{
         return $aSkills;
     }
 
+    function addFigurant($character){
+        $check = $this->checkCardId($character["card_id"]);
+
+        $character_name     = $character["character_name"];
+        $card_id            = $character["card_id"];
+        $faction            = $character["faction"];
+        $rank               = $character["rank"];
+        $threat_assessment  = $character["threat_assessment"];
+        $douane_disposition = $character["douane_disposition"];
+        $douane_notes       = $character["douane_notes"];
+        $bastion_clearance  = $character["bastion_clearance"];
+        $ICC_number         = $character["ICC_number"];
+        $bloodtype          = $character["bloodtype"];
+        $ic_birthday        = $character["ic_birthday"];
+        $homeplanet         = $character["homeplanet"];
+
+        $figustatus = "figurant";
+        if(isset($character["recurring"])){
+            $figustatus = "figurant-recurring";
+        }
+
+        if(!$check){
+
+            $stmt = database::$conn->prepare(
+                "INSERT into ecc_characters
+                    (accountID, character_name, card_id, faction, status, rank, threat_assessment, douane_disposition, douane_notes, bastion_clearance, ICC_number, bloodtype, ic_birthday, homeplanet)
+                VALUES
+                    (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                );
+            $res = $stmt->execute(array(
+                0, $character_name, $card_id, $faction, $figustatus, $rank, $threat_assessment, $douane_disposition, $douane_notes, $bastion_clearance, $ICC_number, $bloodtype, $ic_birthday, $homeplanet
+            ));
+
+            return database::$conn->lastInsertId();
+
+        }else{
+            if($check["status"] == "figurant-recurring"){
+
+                $stmt = database::$conn->prepare("UPDATE ecc_characters SET card_id=? WHERE characterID = ?");
+                $res = $stmt->execute(array(NULL, $check["characterID"]));
+
+                $stmt = database::$conn->prepare(
+                    "INSERT into ecc_characters
+                        (accountID, character_name, card_id, faction, status, rank, threat_assessment, douane_disposition, douane_notes, bastion_clearance, ICC_number, bloodtype, ic_birthday, homeplanet)
+                    VALUES
+                        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                    );
+                $res = $stmt->execute(array(
+                    0, $character_name, $card_id, $faction, $figustatus, $rank, $threat_assessment, $douane_disposition, $douane_notes, $bastion_clearance, $ICC_number, $bloodtype, $ic_birthday, $homeplanet
+                ));
+
+                return database::$conn->lastInsertId();
+
+            }else{
+
+                $stmt = database::$conn->prepare(
+                    "UPDATE ecc_characters SET
+                        character_name=?,
+                        faction=?,
+                        status=?,
+                        rank=?,
+                        threat_assessment=?,
+                        douane_disposition=?,
+                        douane_notes=?,
+                        bastion_clearance=?,
+                        ICC_number=?,
+                        bloodtype=?,
+                        ic_birthday=?,
+                        homeplanet=?
+                    WHERE characterID = ?");
+                $res = $stmt->execute(array($character_name, $faction, $figustatus, $rank, $threat_assessment, $douane_disposition, $douane_notes, $bastion_clearance, $ICC_number, $bloodtype, $ic_birthday, $homeplanet, $check["characterID"]));
+
+                return "success";
+            }
+        }
+
+    }
+
+    private function checkCardId($cardId){
+        $stmt = database::$conn->prepare("SELECT * FROM ecc_characters WHERE card_id = ?");
+        $res = $stmt->execute(array($cardId));
+        $res = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $res;
+    }
 }
