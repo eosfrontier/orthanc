abd418706fbd12d40466110eef78c9919f0ac52e
WafflesMcDuff <goldjd@gmail.com>
2020-11-17 13:28:38 -0500

    Create figurant/_delete.php

---
+++ v2/character/_delete.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!isset($input["id"])) {
+    http_response_code(400);
+    echo json_encode("You haven't include an id to delete.");
+    die();
+} else {
+    $deleteFigu = $cCharacter->deleteCharacter($input["id"]);
+    if ($deleteFigu == 0) {
+        http_response_code(404);
+        echo json_encode("No Characters deleted. Either specified id is not a Character, or they were already deleted.");
+        die();
+    } else {
+        http_response_code(200);
+        echo json_encode($deleteFigu . ' Characters deleted.');
+        die();
+    }
+}
