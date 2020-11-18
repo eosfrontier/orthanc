d8b1f4ca144bae5315d42f012b77b011437a70e0
WafflesMcDuff <goldjd@gmail.com>
2020-11-18 09:08:10 -0500

    Fix missing token response

--- includes/token.php
+++ includes/token.php
@@ -6,11 +6,12 @@ $headers = getallheaders();
 $access = '';
 if ( isset( $headers['token'] ) ) {
 	$access = $headers['token'];
-} else {
+}
+elseif ( isset( $input['token'] ) ) {
 	$access = token( $input['token'] );
 }
 
-if ( $access === false ) {
+else {
 	http_response_code( 401 );
 	echo json_encode( 'YOU SHALL NOT PASS!!' );
 	die();
