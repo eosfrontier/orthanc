45d75e08f805eb31bc2271c75fea848cb670e0c7
Josh G <goldjd@gmail.com>
2021-01-14 23:56:19 -0500

    Update _get.php

--- v2/figurant/namelists/_get.php
+++ v2/figurant/namelists/_get.php
@@ -5,27 +5,27 @@ switch ( $faction ) {
 	case 'aquila':
 		// Get the contents of the JSON file 
 		$strJsonFileContents = file_get_contents("/var/eos_apps/PersonaGenerator/namelists/aquila.json");
-		return $strJsonFileContents;
+		echo $strJsonFileContents;
 		break;
 	case 'dugo':
 		// Get the contents of the JSON file 
 		$strJsonFileContents = file_get_contents("/var/eos_apps/PersonaGenerator/namelists/dugo.json");
-		return $strJsonFileContents;
+		echo $strJsonFileContents;
 		break;
 	case 'ekanesh':
 		// Get the contents of the JSON file 
 		$strJsonFileContents = file_get_contents("/var/eos_apps/PersonaGenerator/namelists/ekanesh.json");
-		return $strJsonFileContents;
+		echo $strJsonFileContents;
 		break;
 	case 'pendzal':
 		// Get the contents of the JSON file 
 		$strJsonFileContents = file_get_contents("/var/eos_apps/PersonaGenerator/namelists/pendzal.json");
-		return $strJsonFileContents;
+		echo $strJsonFileContents;
 		break;
 	case 'sona':
 		// Get the contents of the JSON file 
 		$strJsonFileContents = file_get_contents("/var/eos_apps/PersonaGenerator/namelists/ekanesh.json");
-		return $strJsonFileContents;
+		echo $strJsonFileContents;
 		break;
 	}
 
