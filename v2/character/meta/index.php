546fb4327ef8e51b817fc62fc8d3a11c0409579b
Josh G <goldjd@gmail.com>
2021-01-17 18:49:40 -0500

    give 406 response for wrong characater type for endpoint

--- v2/character/meta/index.php
+++ v2/character/meta/index.php
@@ -4,9 +4,9 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/orthanc/includes/token.php';
 
 $c_meta     = new meta();
 $sheet_type = $c_meta->get_char_type_by_id( $input['id'] );
-if ( strpos( $sheet_type, 'figurant' ) !== false ) {
-	http_response_code( 404 );
-	echo 'No result found';
+if ( (strpos( $sheet_type, 'figurant' ) !== false) ) {
+	http_response_code( 406 );
+	echo json_encode("CharacterID ".$input['id']." is a figurant.");
 	exit;
 }
 else {
--- v2/figurant/meta/index.php
+++ v2/figurant/meta/index.php
@@ -5,8 +5,8 @@ $c_meta     = new meta();
 
 $sheet_type = $c_meta->get_char_type_by_id( $input['id'] );
 if ( !(strpos( $sheet_type, 'figurant' ) !== false) ) {
-	http_response_code( 404 );
-	echo 'No result found';
+	http_response_code( 406 );
+	echo json_encode("CharacterID ".$input['id']." is not a figurant.");
 	exit;
 }
 else {
