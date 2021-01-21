79198006138748a9c46ca8ac87a936eefd37f8c9
Josh G <goldjd@gmail.com>
2021-01-21 00:04:20 -0500

    Add character/skills_v2 GET, DELETE

---
+++ includes/classes/skillsv2.php
@@ -0,0 +1,60 @@
+<?php
+
+class skillsv2
+{
+
+	function get_char_type_by_id($id)
+	{
+		$stmt = database::$conn->prepare('SELECT status FROM ecc_characters WHERE characterID = ? AND sheet_status != "deleted"');
+		$res  = $stmt->execute([$id]);
+		$res  = $stmt->fetchColumn();
+
+		return $res;
+	}
+
+	public function get_skills($id)
+	{
+		$response = array();
+		$stmt        = database::$conn->prepare('SELECT * FROM ecc_char_skills_v2 WHERE charID = ? ORDER BY skill_ID');
+		$res         = $stmt->execute([$id]);
+		$a_char_skills = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		
+		foreach ($a_char_skills as $a_char_skill) {
+			$result = new stdClass();
+			$level = $a_char_skill['level'];
+			if ($level > 5) {
+				$skilllevel = $level - 5;
+			} else {
+				$skilllevel = $level;
+			}
+			$stmt2 = database::$conn->prepare('SELECT * from ecc_skills WHERE skill_id = '.$a_char_skill['skill_id']);
+			$res2 = $stmt2->execute();
+			$res2 = $stmt2->fetcHAll(PDO::FETCH_ASSOC);
+			foreach($res2 as $skill){
+			$result->name=$skill['name'];
+			$result->level = $level;
+			$result->level_name = $skill["level_".$skilllevel."_name"];
+			$result->psychic=$skill['psychic'];
+			$result->statuss=$skill['status'];
+			if($level > 5) {
+				$result->specialty = true;
+			} else {
+				$result->specialty = false;
+			}
+			$result->description =  $skill["level_".$skilllevel."_description"];
+			$result->parents=$skill['parents'];
+			$response = $response + array($a_char_skill['skill_id']=>$result);
+		}
+	}
+
+		return $response;
+	}
+
+	public function del_skill($char_id, $skill_id)
+	{
+		$stmtfinal = database::$conn->prepare("DELETE FROM ecc_char_skills_v2 WHERE charID = $char_id AND skill_id = $skill_id");
+		$resfinal   = $stmtfinal->execute();
+		$count = $stmtfinal->rowCount();
+		return $count;
+	}
+}
---
+++ v2/character/skills_v2/README.md
@@ -0,0 +1,27 @@
+# orthanc/v2/character/skills_v2
+
+| Method | Description                            |
+| ------ | -------------------------------------- |
+| GET    | Gets skills for **id**                 |
+<!-- | POST   | Adds **skills** for **id**             | -->
+| DELETE | Removes **skills** from **id**         |
+<!-- | PUT    | Replaces value of **skills** on **id** | -->
+
+## Resource URL
+orthanc/v2/character/skills_v2
+
+## Resource Information
+|                          |      |
+| ------------------------ | ---- |
+| Response formats         | JSON |
+| Requires authentication? | Yes  |
+| Rate limited?            | No   |
+
+## Parameters
+| Name     | Required | Description                           | Required For | Optional For | Example  |
+| -------- | -------- | ------------------------------------- | ------------ | ------------ | -------- |
+| token    | yes      | provides authentication               | All Methods  |              | q1w2e3r4 |
+| id       | yes      | Character ID to add/update skills for | All Methods  |              | 42       |
+| skill_id | yes      | skill_id to maniplate for character   | DELETE       | 31014        |          |
+
+
