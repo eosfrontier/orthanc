8a154f23fff1baff3fb4ec65daecc2da4ec9cdd0
Josh G <goldjd@gmail.com>
2021-01-21 09:09:16 -0500

    Update _get.php

--- v2/character/skills_v2/_get.php
+++ v2/character/skills_v2/_get.php
@@ -5,8 +5,10 @@ if ( empty( $input['id'] ) && !isset($input['get_all']) ) {
 	echo json_encode( "You haven't included an 'id' or 'get_all'." );
 	die();
 }
-
-$id = $input['id'];
+if(isset($input['id']))
+{
+	$id = $input['id'];
+}
 
 if( isset($input['get_all'])){
 	$a_skills = $c_fetch->get_skills_all_chars();
