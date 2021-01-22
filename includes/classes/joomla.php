2664e9e7bee7df2343c0830ca046dd9c6d9adfb6
Josh G <goldjd@gmail.com>
2021-01-21 22:43:11 -0500

    Revert "Move joomla includes"
    This reverts commit 778523cf8531fedb7abbf94ddc361ca505cf6e7e.

--- includes/classes/joomla.php
+++ includes/classes/joomla.php
@@ -1,6 +1,10 @@
 <?php
+define('_JEXEC', 1);
+define('JPATH_BASE', '/var/www/html/');
+require_once JPATH_BASE . '/includes/defines.php';
+require_once JPATH_BASE . '/includes/framework.php';
 $app = JFactory::getApplication('site');
-                                            $user = JFactory::getUser();
+$user = JFactory::getUser();
 
 class joomla {
 // Required Files
