2614c43e4fb52c0cb5d8e3b2948246ae4927b584
Rolf <62988563+rolf-yoast@users.noreply.github.com>
2021-03-01 22:54:07 +0100

    Update index.php

--- v2/bank/index.php
+++ v2/bank/index.php
@@ -7,9 +7,6 @@ switch ( $method ) {
 	case 'POST':
 		require_once './_post.php';
 		break;
-	case 'PUT':
-		require_once './_put.php';
-		break;
 	case 'GET':
 		require_once './_get.php';
 		break;
