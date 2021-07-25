dd2d388d13785ac59bc66c6b2325a4e692e7ff1a
WafflesMcDuff <goldjd@gmail.com>
2021-07-25 11:56:46 +0200

    Bunch of small fixes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # orthanc
 all new integrations should use [orthanc v2](./v2/README.md)
+
+
+## Notes on local setup
+ - When downloading repo for the first time, you must run `composer install`
+ - 
--- includes/token.php
+++ includes/token.php
@@ -1,14 +1,15 @@
 <?php
+
 $token_table = 'eos_tokens';
 function token( $token, $token_table ) {
-	$stmt = database::$conn->prepare( "SELECT * FROM $token_table WHERE token = ?" );
-	$res  = $stmt->execute( [ $token ] );
-	$res  = $stmt->fetch( PDO::FETCH_ASSOC );
+	$stmt  = database::$conn->prepare( "SELECT * FROM $token_table WHERE token = ?" );
+	$res   = $stmt->execute( [ $token ] );
+	$res   = $stmt->fetch( PDO::FETCH_ASSOC );
 	$count = $stmt->rowCount();
 
 	if ( $count > 0 ) {
 		return $res['name'];
-	} 
+	}
 	else {
 		return false;
 	}
@@ -20,7 +21,7 @@ $headers = getallheaders();
 $access = '';
 if ( isset( $headers['token'] ) ) {
 	$access = token( $headers['token'], $token_table );
-	if ($access == false) {
+	if ( $access == false ) {
 		http_response_code( 401 );
 		echo json_encode( 'YOU SHALL NOT PASS!!' );
 		die();
@@ -28,7 +29,7 @@ if ( isset( $headers['token'] ) ) {
 }
 elseif ( isset( $input['token'] ) ) {
 	$access = token( $input['token'], $token_table );
-	if ($access == false) {
+	if ( $access == false ) {
 		http_response_code( 401 );
 		echo json_encode( 'YOU SHALL NOT PASS!!' );
 		die();
