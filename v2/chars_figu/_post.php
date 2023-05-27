9ce718b245dcd48c58885fef0f2a1f1a304351f0
WafflesMcDuff <goldjd@gmail.com>
2023-05-27 13:26:39 +0200

    Remove debug

--- v2/chars_figu/_post.php
+++ v2/chars_figu/_post.php
@@ -1,9 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-
-ini_set('display_startup_errors', 1);
-
-error_reporting(E_ALL);
 if ( empty( $input['figurant'] ) ) {
 	http_response_code( 400 );
 	echo json_encode( "You haven't included all needed info." );
