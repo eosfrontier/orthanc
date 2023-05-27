3984e4c207c6d4cdfaad80d066f7174c52470ea2
WafflesMcDuff <goldjd@gmail.com>
2023-05-27 13:26:22 +0200

    Add UTF8Encode

--- v2/chars_figu/_post.php
+++ v2/chars_figu/_post.php
@@ -1,12 +1,16 @@
 <?php
+ini_set('display_errors', 1);
 
+ini_set('display_startup_errors', 1);
+
+error_reporting(E_ALL);
 if ( empty( $input['figurant'] ) ) {
 	http_response_code( 400 );
 	echo json_encode( "You haven't included all needed info." );
 	die();
 }
 
-$a_figurant = json_decode( $input['figurant'], true );
+$a_figurant = json_decode( utf8_encode( $input['figurant']), true );
 
 $a_result = $c_fetch->add_figurant( $a_figurant );
 http_response_code( 200 );
