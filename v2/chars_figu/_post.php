ce1311712454065a9fc54213ef7cfb66a5dee5f9
Josh G <goldjd@gmail.com>
2023-08-10 13:52:10 +0200

    Bug fix/special characters in new figu name (#61)
    * Add UTF8Encode
    
    * Remove debug
    
    * Problem solved now?
    
    * refactor the fix
    
    * fix typo
    
    * See if this is still a fix
    
    * See if this is still a fix
    
    * Save killyourdarlings for a feature branch
    
    * Update Char_Figu.php

--- v2/chars_figu/_post.php
+++ v2/chars_figu/_post.php
@@ -1,12 +1,11 @@
 <?php
-
 if ( empty( $input['figurant'] ) ) {
 	http_response_code( 400 );
 	echo json_encode( "You haven't included all needed info." );
 	die();
 }
 
-$a_figurant = json_decode( $input['figurant'], true );
+$a_figurant = json_decode( utf8_encode( $input['figurant']), true );
 
 $a_result = $c_fetch->add_figurant( $a_figurant );
 http_response_code( 200 );
