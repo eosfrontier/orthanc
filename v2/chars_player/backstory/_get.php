ba411fe970b39d213411705fcd427a5c1663b6ef
mrperson123 <mrperson123@users.noreply.github.com>
2023-08-12 10:28:21 +0200

    Tests

--- v2/chars_player/backstory/_get.php
+++ v2/chars_player/backstory/_get.php
@@ -8,9 +8,10 @@ if ( !isset( $input['char_id'] ) || !isset( $input['type'] ) ) {
 }
 
 else {
-	if ($input['type'] != 'concept' && $input['type'] != 'backstory'){
+	$types = ['concept', 'backstory', 'concept_changes' ,'backstory_changes'];
+	if ( ! in_array( $input['type'], $types ) ){
 		http_response_code( 400 );
-		die(json_encode("Invalid type. 'type' must be 'concept' or 'backstory'"));
+		die(json_encode("Invalid type. 'type' must be 'concept' or 'backstory'1"));
 	}
 
 	if ( $input['char_id'] == 'all' ){
--- v2/chars_player/backstory/_put.php
+++ v2/chars_player/backstory/_put.php
@@ -7,7 +7,7 @@ else {
 	$types = ['concept', 'backstory', 'concept_changes' ,'backstory_changes'];
 	if ( ! in_array( $input['type'], $types ) ){
 		http_response_code( 400 );
-		die(json_encode("Invalid type. 'type' must be 'concept' or 'backstory'"));
+		die(json_encode("Invalid type. 'type' must be 'concept' or 'backstory'2"));
 	}
 	$a_result = $c_fetch->set_backstory( $input['char_id'], $input['type'], $input['content'] );
 	http_response_code( 200 );
