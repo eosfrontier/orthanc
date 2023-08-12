5b77d114d0fd23097ace927990ec23ae44fc6907
mrperson123 <mrperson123@users.noreply.github.com>
2023-08-12 10:26:41 +0200

    Update _get.php

--- v2/chars_player/backstory/statuses/_get.php
+++ v2/chars_player/backstory/statuses/_get.php
@@ -8,7 +8,8 @@ if ( !isset( $input['type'] ) ) {
 }
 
 else {
-	if ($input['type'] != 'concept' && $input['type'] != 'backstory'){
+	$types = ['concept', 'backstory', 'concept_changes' ,'backstory_changes'];
+	if ( ! in_array( $input['type'], $types ) ){
 		http_response_code( 400 );
 		die(json_encode("Invalid type. 'type' must be 'concept' or 'backstory'"));
 	}
