2f7025594586f97534dfb8650848ecf889a933bf
WafflesMcDuff <goldjd@gmail.com>
2026-07-09 21:48:11 +0200

    Fix getall for Shuttlebay

--- v2/shuttlebay/_get.php
+++ v2/shuttlebay/_get.php
@@ -1,14 +1,14 @@
 <?php
 
-if (!isset($input['id']) && !isset($input['getAll'])) {
+if (!isset($input['id']) && !isset($input['getall'])) {
 	http_response_code(400);
-	die(json_encode("You must include a 'id' OR 'getAll'"));
-} elseif (isset($input['id']) && isset($input['getAll'])) {
+	die(json_encode("You must include a 'id' OR 'getall'"));
+} elseif (isset($input['id']) && isset($input['getall'])) {
 	http_response_code(400);
-	die(json_encode("You must include ONLY an 'id' OR 'getAll'"));
+	die(json_encode("You must include ONLY an 'id' OR 'getall'"));
 } elseif (isset($input['id'])) {
 	$response = $shuttlebay->getShuttle(($input['id']));
-} elseif (isset($input['getAll'])) {
+} elseif (isset($input['getall'])) {
 	$response = $shuttlebay->getAllShuttles();
 }
 if (empty($response)) {
